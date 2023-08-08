3e581d805479f81085f5a83004abb52bcd83b202
“Narayanan <narayanan.sundaram@anywhere.co>
2023-08-08 11:50:27 +0530

    debouncing added

---
+++ src/emailData.js
@@ -0,0 +1,120 @@
+const jsonData  = {
+    "emails": [
+        {
+            "id": 1,
+            "subject": "Design Language",
+            "profileImage": "https://i.postimg.cc/zBtyMs3k/man.webp",
+            "name": "Salman Shah",
+            "email": "salmanshah@gmail.com",
+            "attachment": true,
+            "fetchTime": "08:00 PM, Today",
+            "content": "Hello Salman,<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.<br><br>Cheers...",
+            "fileName": "Selector.pdf",
+            "fileType":"pdf"
+    
+        },
+        {
+            "id": 2,
+            "subject": "Nextmail New Look",
+            "profileImage": "https://i.postimg.cc/nVRbhcdJ/group-11.webp",
+            "name": "Christian Bale",
+            "email": "christian.bale@gmail.com",
+            "attachment": true,
+            "fetchTime": "11:00 AM, Today",
+            "content": "Hello Christian,<br><br>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', <br><br>Making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <br><br>Cheers...",
+            "fileName": "Get.word",
+            "fileType": "word"
+        },
+        {
+            "id": 3,
+            "subject": "Interview with Shopify",
+            "profileImage": "https://i.postimg.cc/L8FLSM69/user.webp",
+            "name": "Aisha Dialo",
+            "email": "aisha@gmail.com",
+            "attachment": false,
+            "fetchTime": "12:05 PM, Today",
+            "content": "Hello Aisha,<br><br>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10., comes from a line in section 1.10.32.<br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.<br><br>Cheers",
+        },
+        {
+            "id": 4,
+            "subject": "Narcos Season 2",
+            "profileImage": "https://i.postimg.cc/fTGjb95h/profile.webp",
+            "name": "Netflix",
+            "email": "netflix@gmail.com",
+            "attachment": true,
+            "fetchTime": "07:35 PM, Today",
+            "content": "Hello Netflix,<br><br>Long established fact that a reader will be distracted by the ,Making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. <br><br> Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br><br>Cheers...",
+            "fileName": "Content.word",
+            "fileType": "word"
+        },
+        {
+            "id": 5,
+            "subject": "Top Stories of the Day",
+            "profileImage": "https://i.postimg.cc/0yYd3Nvd/bussiness-man.webp",
+            "name": "TechCrunch",
+            "email": "TechCrunch@gmail.com",
+            "attachment": false,
+            "fetchTime": "Yesterday",
+            "content": "Hello TechCrunch,<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.<br><br>Cheers...",
+        },
+        {
+            "id": 6,
+            "subject": "Updates from Kevin",
+            "profileImage": "https://i.postimg.cc/zBtyMs3k/man.webp",
+            "name": "Goodreads",
+            "email": "Goodreads@gmail.com",
+            "attachment": true,
+            "fetchTime": "Yesterday",
+            "content": "Hello Goodreads,<br><br>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10., comes from a line in section 1.10.32.<br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.<br><br>Cheers",
+            "fileName": "project.pdf",
+            "fileType":"pdf"
+        },
+        {
+            "id": 7,
+            "subject": "Startup 2016",
+            "profileImage": "	https://i.postimg.cc/fTGjb95h/profile.webp",
+            "name": "Arjun",
+            "email": "arjun@gmail.com",
+            "attachment": false,
+            "fetchTime": "07:35 PM, Today",
+            "content": "Hello Arjun,<br><br>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt <br><br>in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est.<br><br>Cheers...",
+        },
+        {
+            "id": 8,
+            "subject": "Project-11",
+            "profileImage": "https://i.postimg.cc/j22vL0mH/IMG-20230605-200329-1.webp",
+            "name": "Naren",
+            "email": "naren123@gmail.com",
+            "attachment": true,
+            "fetchTime": "09 Aug, 19",
+            "content": "Hello Naren,<br><br>Long established fact that a reader will be distracted by the ,  now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.<br><br>Making it look like readable English. Many desktop publishing packages and web page editors Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br><br>Cheers..",
+            "fileName": "Details.word",
+            "fileType":"word"
+        },
+        {
+            "id": 9,
+            "subject": "Startup 2017",
+            "profileImage": "https://i.postimg.cc/L8FLSM69/user.webp",
+            "name": "Karthick",
+            "email": "karthick@gmail.com",
+            "attachment": false,
+            "fetchTime": "12 Oct, 19",
+            "content": "Hello Karthick,<br><br>We denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; <br><br>and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.These cases are perfectly simple and easy to distinguish.<br><br>Cheers...",
+        },
+        {
+            "id": 10,
+            "subject": "Development Process",
+            "profileImage": "https://i.postimg.cc/zBtyMs3k/man.webp",
+            "name": "Suryakumar",
+            "email": "Suryakumar@gmail.com",
+            "attachment": true,
+            "fetchTime": "20 Mar, 20",
+            "content": "Hello Goodreads,<br><br>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10., comes from a line in section 1.10.32.<br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.<br><br>Cheers",
+            "fileName": "Development.pdf",
+            "fileType":"pdf"
+        },
+    ]
+}
+
+export default jsonData;
+
---
+++ src/script.js
@@ -0,0 +1,135 @@
+import './sass/main.scss';
+import jsonData from './emailData';
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Searching functionality
+    const searchInput = document.querySelector('.search-input');
+    const emailContainers = document.querySelectorAll('.item');
+    const noMatchingMessage = document.querySelector('.no-matching-message');
+    
+    searchInput.addEventListener('keyup', () => {
+        const filter = searchInput.value.toLowerCase();
+        let hasMatch = false;
+    
+        emailContainers.forEach(container => {
+            const subject = container.getAttribute('data-subject');
+            const name = container.getAttribute('data-name');
+    
+            if ((subject && subject.toLowerCase().includes(filter)) || (name && name.toLowerCase().includes(filter))) {
+                container.style.display = 'flex';
+                hasMatch = true;
+            } else {
+                container.style.display = 'none';
+            }
+        });
+    
+        if (hasMatch) {
+            noMatchingMessage.style.display = 'none';
+        } else {
+            noMatchingMessage.style.display = 'block';
+        }
+    });
+    
+    
+    //It will add the file icon based on the type.
+    function getAttachmentIconSrc(fileType) {
+        switch (fileType) {
+            case 'pdf':
+                return 'https://i.postimg.cc/Y9JcNL75/pdf.png'; 
+            case 'word':
+                return 'https://i.postimg.cc/15WdDYQk/word.png'; 
+            default:
+                return 'None'; 
+        }
+    }
+
+    function displayEmailContent(emailId) {
+        const email = jsonData.emails.find(email => email.id === parseInt(emailId));
+
+        if (email) {
+            
+            const contentDiv = document.getElementById('emailList');
+            contentDiv.innerHTML = `
+                    <h2 class="sub">${email.subject}</h2>
+                    <div class="profile-card">
+                        <div class="small-profile-details">
+                            <img src="${email.profileImage}" alt="profile" class="small-profile">
+                            <div class="user-details">
+                                <h2 class="user-name">${email.name}</h2>
+                                <p class="mail">${email.email}</p>
+                            </div>
+                        </div>
+                        <span class="fetch-time">${email.fetchTime}</span>
+                    </div>
+                    <p class="data-content">${email.content}</p>
+                    <hr class="line">
+                    ${email.attachment ? `
+                    <div class="file-container">
+                        <div class="file-icon">
+                            <img src="${getAttachmentIconSrc(email.fileType)}" alt="${email.fileType}">
+                        </div>
+                        <p class="file-name">${email.fileName}</p>
+                    </div>
+                ` : ''}
+                `;
+            const profileDetails = document.querySelector('.small-profile-details');
+            const profileImage = profileDetails.querySelector('.small-profile');
+            const userName = profileDetails.querySelector('.user-name');
+            const userEmail = profileDetails.querySelector('.mail');
+
+            profileImage.src = email.profileImage;
+            userName.textContent = email.name;
+            userEmail.textContent = email.email;
+         
+
+
+        }
+    }
+
+
+    // Get all flex-container elements with dot and border while clicking
+    const flexContainers = document.querySelectorAll('.item');
+    flexContainers[1].style.backgroundColor = "white";
+    flexContainers[1].style.borderLeft = "3px solid #379aff";
+    let lastClickedContainer = flexContainers[1];
+    flexContainers.forEach((container, index) => {
+        
+        const timeAttachmentDiv = container.querySelector('.time-attachment');
+        if (timeAttachmentDiv) {
+            const attachmentIconDiv = timeAttachmentDiv.querySelector('.attachment');
+            if (attachmentIconDiv) {
+                const emailId = container.getAttribute('data-email-id');
+                const email = jsonData.emails.find(email => email.id === parseInt(emailId));
+    
+                if (email && email.attachment) {
+                    attachmentIconDiv.style.display = 'block';
+                } else {
+                    attachmentIconDiv.style.display = 'none';
+                }
+            }
+        }
+        if (index > 0) {
+            container.addEventListener('click', () => {
+                const emailId = container.getAttribute('data-email-id');
+                displayEmailContent(emailId);
+                const blueDot = container.querySelector('.blue-dot');
+                const subject = container.querySelector('.subject');
+                if (blueDot) {
+                    blueDot.style.visibility = 'hidden';
+                }
+                if (lastClickedContainer) {
+                    lastClickedContainer.style.backgroundColor = '';
+                    lastClickedContainer.style.borderLeft = "3px solid transparent";
+                }
+                container.style.backgroundColor = 'white';
+                container.style.borderLeft = '3px solid #379aff'
+                lastClickedContainer = container;
+             
+            });
+
+        }
+    });
+    window.addEventListener('load', () => {
+        displayEmailContent("1");
+    });
+})
