2d625badfd8e9aa52f279688a0791a943e16fb4d
“Narayanan <narayanan.sundaram@anywhere.co>
2023-08-08 11:57:44 +0530

    debouncing added

--- src/script.js
+++ src/script.js
@@ -6,15 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.querySelector('.search-input');
     const emailContainers = document.querySelectorAll('.item');
     const noMatchingMessage = document.querySelector('.no-matching-message');
-    
+
     searchInput.addEventListener('keyup', () => {
+
         const filter = searchInput.value.toLowerCase();
         let hasMatch = false;
-    
+
         emailContainers.forEach(container => {
             const subject = container.getAttribute('data-subject');
             const name = container.getAttribute('data-name');
-    
+
             if ((subject && subject.toLowerCase().includes(filter)) || (name && name.toLowerCase().includes(filter))) {
                 container.style.display = 'flex';
                 hasMatch = true;
@@ -22,24 +23,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 container.style.display = 'none';
             }
         });
-    
+
         if (hasMatch) {
             noMatchingMessage.style.display = 'none';
         } else {
             noMatchingMessage.style.display = 'block';
         }
     });
-    
+
+    function getData() {
+        let inpVal = searchInput.value;
+        console.log(inpVal);
+    }
+
+    function debouncing(func, delay) {
+        let timer;
+        return function () {
+            let context = this;
+            let args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                func.apply(context, args);
+            }, delay);
+        }
+    }
+
+    const better = debouncing(getData, 300);
+    searchInput.addEventListener("keyup", better);
     
     //It will add the file icon based on the type.
     function getAttachmentIconSrc(fileType) {
         switch (fileType) {
             case 'pdf':
-                return 'https://i.postimg.cc/Y9JcNL75/pdf.png'; 
+                return 'https://i.postimg.cc/Y9JcNL75/pdf.png';
             case 'word':
-                return 'https://i.postimg.cc/15WdDYQk/word.png'; 
+                return 'https://i.postimg.cc/15WdDYQk/word.png';
             default:
-                return 'None'; 
+                return 'None';
         }
     }
 
@@ -47,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = jsonData.emails.find(email => email.id === parseInt(emailId));
 
         if (email) {
-            
+
             const contentDiv = document.getElementById('emailList');
             contentDiv.innerHTML = `
                     <h2 class="sub">${email.subject}</h2>
@@ -80,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
             profileImage.src = email.profileImage;
             userName.textContent = email.name;
             userEmail.textContent = email.email;
-         
+
 
 
         }
@@ -93,14 +113,14 @@ document.addEventListener('DOMContentLoaded', () => {
     flexContainers[1].style.borderLeft = "3px solid #379aff";
     let lastClickedContainer = flexContainers[1];
     flexContainers.forEach((container, index) => {
-        
+
         const timeAttachmentDiv = container.querySelector('.time-attachment');
         if (timeAttachmentDiv) {
             const attachmentIconDiv = timeAttachmentDiv.querySelector('.attachment');
             if (attachmentIconDiv) {
                 const emailId = container.getAttribute('data-email-id');
                 const email = jsonData.emails.find(email => email.id === parseInt(emailId));
-    
+
                 if (email && email.attachment) {
                     attachmentIconDiv.style.display = 'block';
                 } else {
@@ -124,7 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 container.style.backgroundColor = 'white';
                 container.style.borderLeft = '3px solid #379aff'
                 lastClickedContainer = container;
-             
+
             });
 
         }
